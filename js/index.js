6602fb2223f28e0cc6ecf9ff6105b0322fee0908
Taegwan Youn <tkyoun0421@naver.com>
2022-06-10 10:27:31 +0900

    Modified function window resize

--- js/index.js
+++ js/index.js
@@ -44,7 +44,9 @@ $(function(){
     let bannerIndex = 0;
     let productPage = 0;
     let monthProductPage = 0;
-    let desktopVer = 0;
+    let device = 'pc';
+    let timer = 'null';
+    let sec = 150;
 
     // 기본 세팅
     $(document).ready(function(){
@@ -196,37 +198,56 @@ $(function(){
     }
 
     function windowResize (){
-        if ($(window).width() <= 750) {
-            desktopVer = false;
-            if (desktopVer == false) {
-                bannerArticle.css({
-                    width: 100 + '%'
-                });
-                if (productPage >= 3) {
-                    productPage = 1;
-                    newProductGroup.css({
-                        marginLeft: productPage * -100 + '%'
+        clearTimeout(timer);
+        timer = setTimeout(function(){
+            if ($(window).width() <= 750) {
+                if (device == "mobile") {
+                    bannerArticle.css({
+                        width: 100 + '%'
                     });
-                    newProductPaging.removeClass('now');
-                    newProductPaging.eq(productPage).addClass('now');
-                }            
-                if (monthProductPage >= 3) {
-                    monthProductPage = 1;
-                    monthProductGroup.css({
-                        marginLeft: monthProductPage * -100 + '%'
+                    if (productPage >= 3) {
+                        productPage = 1;
+                        newProductGroup.css({
+                            marginLeft: productPage * -100 + '%'
+                        });
+                        newProductPaging.removeClass('now');
+                        newProductPaging.eq(productPage).addClass('now');
+                    }            
+                    if (monthProductPage >= 3) {
+                        monthProductPage = 1;
+                        monthProductGroup.css({
+                            marginLeft: monthProductPage * -100 + '%'
+                        });
+                        monthProductPaging.removeClass('now');
+                        monthProductPaging.eq(monthProductPage).addClass('now');
+                    }
+                    device = "pc"
+                }
+            } else if ($(window).width() > 750) {
+                if (device == "pc") {
+                    bannerArticle.css({
+                        width: 'calc(100% / 3 - 10px)'
                     });
-                    monthProductPaging.removeClass('now');
-                    monthProductPaging.eq(monthProductPage).addClass('now');
+                    if (productPage >= 3) {
+                        productPage = 1;
+                        newProductGroup.css({
+                            marginLeft: productPage * -100 + '%'
+                        });
+                        newProductPaging.removeClass('now');
+                        newProductPaging.eq(productPage).addClass('now');
+                    }            
+                    if (monthProductPage >= 3) {
+                        monthProductPage = 1;
+                        monthProductGroup.css({
+                            marginLeft: monthProductPage * -100 + '%'
+                        });
+                        monthProductPaging.removeClass('now');
+                        monthProductPaging.eq(monthProductPage).addClass('now');
+                    }
+                    device = "mobile";
                 }
             }
-        } else if ($(window).width() > 750) {
-            desktopVer = true;
-            if (desktopVer == true) {
-                bannerArticle.css({
-                    width: 'calc(100% / 3 - 10px)'
-                });
-            }
-        }
+        }, sec);
     }
 
     function productSwipeRight (){
